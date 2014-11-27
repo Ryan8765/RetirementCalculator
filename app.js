b1cd8d897a6469a81140f165df48f75519d6347e
Ryan8765 <rmhaas2211@gmail.com>
2014-11-27 13:50:19 -0600

    Finished lump sum calculator

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 		//populate input function
 		function populateInputs(){
 			for(i=0; i<numInputs; i++) {
-				var inputs = $('<div class="tableRow addedNode"><p>Lump sum in investment '+(numInputs-i)+':</p><p><input type="text"></p></div><div class="tableRow addedNode"><p>Interest for investment '+(numInputs-i)+' (percent):</p><p><input type="text"></p></div>');
+				var inputs = $('<div class="tableRow addedNode"><p>Lump sum in investment '+(numInputs-i)+':</p><p><input type="number" class="lump"></p></div><div class="tableRow addedNode"><p>Annual interest for investment '+(numInputs-i)+' (percent):</p><p><input type="number" class="interest"></p></div>');
 				$('#updateWidth .table div').eq(0).after(inputs);
 			}
 		}; //end populate inputs function
@@ -29,7 +29,7 @@ $(document).ready(function() {
 		//Number of investment areas entered by user
 		var numInputs = $('#numInputs').val();
 		//popup html
-		var popupNode = $('<div class="popupBackground" id="popup1"><div class="container"><p>Are you sure you want to re-populate the inputs?<br>All previously entered data for these inputs will be lost</p><div class="outerContainer"><button id="yes">yes</button><button id="no">No</button></div></div></div>');
+		var popupNode = $('<div class="popupBackground" id="popup1"><div class="container"><p>Are you sure you want to repopulate the inputs?<br>All previously entered data for these inputs will be lost</p><div class="outerContainer"><button id="yes">Yes</button><button id="no">No</button></div></div></div>');
 		//if inputs have or have not been populated before how should we handle when "populate input" button is clicked
 		if(populatedInputs) {
 			//show popup warning
@@ -39,6 +39,7 @@ $(document).ready(function() {
 				$('.popupBackground').remove();
 				removeInputs();
 				populateInputs();
+				lumpSumCalc();
 			});
 			$('#popup1 #no').on('click',function(){
 				$('.popupBackground').remove();
@@ -52,8 +53,77 @@ $(document).ready(function() {
 	}); //end populate input button
 
 
+//Calculate lump sums when calculate button is clicked
+	var numInvestments;
+	function lumpSumCalc() {
+		//variables
+		//present values array
+		var pv = [];
+		while(pv.length > 0) {
+    		pv.pop();
+		}
+		//interest arrays divided by 12
+		var r = [];
+		while(r.length > 0) {
+    		r.pop();
+		}
+		//future worth value total
+		var fw = 0;
+		//future worth calculation
+		function futureWorth () {
+			for (i=0; i < numInvestments; i++) {
+				//present value calc
+				var element3 = Math.pow(( 1 + r[i]), n);
+				element3 = pv[i] * element3;
+				//two decimal places
+				fw = fw + element3;
+			}
+		};
+		numInvestments = $('.addedNode').length/2;
+		//user enters years or months
+		var yearsOrMonths = $('#yearsMonths1').val();
+		//number of periods in years
+		var n = $('#numPeriods').val();
+		//Present value lump sums array
+		$('.lump').each(function() {
+			var element = $(this).val();
+			pv.push(element);
+		});
+		//interest lump sums array divided by 12 to compound by month
+		$('.interest').each(function(){
+			var element2 = ($(this).val()/12)/100;
+			r.push(element2);
+		})
+		//if user enters years or if user enters months for calculation change period accordingly.
+		if(yearsOrMonths == "years") {
+			n = n * 12;
+			futureWorth();
+		} else {
+			futureWorth();
+		} //end if
+		//insert answer in html
+		$('#fw').text('$ ' + fw.toFixed(2));
 
 
+		console.log('pv ' + pv);
+		console.log('fw ' + fw);
+		console.log('r ' + r);
+		console.log(numInvestments);
+	};//end lump sum calc
 
+	//when calculate is clicked calculate lump sum and monthly savings
+	$('#calcLumpSums').on('click',function(){
+		lumpSumCalc();
+		$('.one').animate({
+			fontWeight:'900',
+			fontSize:'1.4em'
+			}, 2000,
+			function(){
+				$('.one').animate({
+					fontWeight:'100',
+					fontSize:'1em'
+				}, 1000);
+		});
+	});
 //----------------------------------	
 });
